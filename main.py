580c7aa5a45def0691cc7296e6d370aa46556012
kevin vargas <benji140923@gmail.com>
2025-07-04 14:09:39 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
