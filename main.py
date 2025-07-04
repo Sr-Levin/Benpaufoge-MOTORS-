7cb5b4cc5e35ccc54d359214ecb4d4749a17629d
kevin vargas <benji140923@gmail.com>
2025-07-04 14:03:05 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Bienvenido a BenPauFoge Motors 🚗</h1><p>Donde los motores vuelven a rugir.</p>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
