4582aea7e4f66191c310fd21bc0a531a58b26569
kevin vargas <benji140923@gmail.com>
2025-07-05 20:15:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ WEBHOOK_URL = 'https://discord.com/api/webhooks/1391203448632115323/iIpYNhK7t0G9
 
 def enviar_a_discord(empleado, fecha, vehiculo, trabajo):
     embed = {
-        "title": "🛠️ REPORTE DE TRABAJO DEL ESTADO💸 PAGO INMEDIATO💸",
+        "title": "🛠️ REPORTE DE TRABAJO DEL ESTADO 💸 PAGO INMEDIATO 💸",
         "color": 15158332,
         "fields": [
             {"name": "👨‍🔧 Empleado", "value": empleado, "inline": True},
